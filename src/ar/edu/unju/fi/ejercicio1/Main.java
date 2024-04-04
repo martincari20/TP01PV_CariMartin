d5d655ab311519c6c81e10c34270659159651778
marti <marti@Tolas>
2024-04-04 15:18:58 -0300

    ejercicio1

---
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=5,n2=2,n3=7,n4=18,n5=1;
+		
+		double promedio = (double) (n1+n2+n3+n4+n5)/5;
+		
+		System.out.println(n1+", "+n2+", "+n3+", "+n4+", "+n5);
+		System.out.println("El promedio es: "+promedio);
+		
+	}
+
+}
